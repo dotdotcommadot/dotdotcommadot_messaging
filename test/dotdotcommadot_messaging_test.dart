51e60bec13eba0cd3269de69641acf85e8918474
dotdotcommadot <hans@dotdotcommadot.com>
2014-04-30 19:08:17 +0200

    adds unit tests

--- test/dotdotcommadot_messaging_test.dart
+++ test/dotdotcommadot_messaging_test.dart
@@ -1,8 +1,11 @@
 library dotdotcommadot_messaging_test;
 
 import 'package:unittest/unittest.dart';
+import 'package:dotdotcommadot_messaging/dotdotcommadot_messaging.dart';
+
+part 'testcases/messaging_test_case.dart';
 
 main()
 {
-	//group('Instantiation Tests -', instantiationTestCase);
+	group('Messaging Tests -', messagingTestCase);
 }
---
+++ test/testcases/messaging_test_case.dart
@@ -0,0 +1,33 @@
+part of dotdotcommadot_messaging_test;
+
+messagingTestCase()
+{
+	IMessageDispatcher messageDispatcher;
+	const Symbol STUFF_HAPPENING = const Symbol("stuffHappening");
+	
+	setUp(() 
+	{
+		messageDispatcher = new MessageDispatcher();	
+	});
+	
+	tearDown(() 
+	{
+		messageDispatcher = null;
+	});
+	
+	test('Messaging', () 
+	{
+		Function callback = expectAsync((Message message){ }, count: 1);
+		
+		messageDispatcher.addListener(STUFF_HAPPENING, callback);
+		messageDispatcher.send(STUFF_HAPPENING);
+	});
+
+	test('Messaging Data', () 
+	{
+		Function callback = (Message message){ expect(message.data, equals(" Message One")); };
+		
+		messageDispatcher.addListener(STUFF_HAPPENING, callback);
+		messageDispatcher.send(STUFF_HAPPENING, " Message One");
+	});
+}
